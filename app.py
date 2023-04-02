125056cc6bc99656864bec80da2c1ac3acee6150
jvill171 <juanvillarreal64@yahoo.com>
2023-04-02 11:58:14 -0500

    Added Post model

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 """Blogly application."""
-
 from flask import Flask, request, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.app_context().push()
